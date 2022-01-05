c3872cc53ea3ccabeff0fe5aa3a7ca40dda138b6
julian-dotcom <74622908+julian-dotcom@users.noreply.github.com>
2022-01-05 12:55:48 -0500

    removed stuff

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,8 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0xa04384CF67be44A6D5f4870599f29A46fbc7Ea03'
-    // '0xef4066c45ac32908b1825d27A17D0f75D1593A20' OLD CONTRACT
+    '0xcDdF168808b9172CA63536DF5897f7021DBb947D'
 );
 
 export default instance;
